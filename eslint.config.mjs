d263f2bd59b557e09321dbe21673a5f0714a2de3
Daniel-k-dev <juyeon10120@naver.com>
2025-11-08 18:34:05 +0900

    nestjs 프로젝트 초기 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,69 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config([
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: 'multiline-block-like', next: 'const' },
+        { blankLine: 'always', prev: 'const', next: 'multiline-block-like' },
+        {
+          blankLine: 'always',
+          prev: 'multiline-block-like',
+          next: 'multiline-block-like',
+        },
+
+        { blankLine: 'always', prev: 'class', next: 'multiline-block-like' },
+        { blankLine: 'always', prev: 'multiline-block-like', next: 'class' },
+        { blankLine: 'always', prev: 'class', next: 'class' },
+
+        { blankLine: 'always', prev: 'class', next: 'const' },
+        { blankLine: 'always', prev: 'const', next: 'class' },
+
+        { blankLine: 'always', prev: 'class', next: 'multiline-block-like' },
+        { blankLine: 'always', prev: 'multiline-block-like', next: 'class' },
+        { blankLine: 'always', prev: 'class', next: 'class' },
+      ],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+  // 엔티티, DTO
+  {
+    files: ['**/*.entity.ts', '**/*.dto.ts'],
+    rules: {
+      'lines-between-class-members': [
+        'warn',
+        {
+          enforce: [{ blankLine: 'always', prev: '*', next: '*' }],
+        },
+        { exceptAfterSingleLine: false },
+      ],
+    },
+  },
+]);
