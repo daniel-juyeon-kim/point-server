76fe57c094bd2abaf771df22a24f3483487d6c31
Daniel-k-dev <juyeon10120@naver.com>
2025-11-09 11:58:38 +0900

    feat(config): TypeORM 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiModule } from './api/api.module';
+import { databaseConfig, rootConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ApiModule,
+    rootConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(databaseConfig)],
+      inject: [databaseConfig.KEY],
+      useFactory: (dbConfig: ConfigType<typeof databaseConfig>) => ({
+        ...dbConfig,
+        entities: [__dirname + '/api/**/*.entity.{ts,js}'],
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
