c6e416779b2dfca134fd3281aaee00c44b78300b
Daniel-k-dev <juyeon10120@naver.com>
2025-11-11 21:01:34 +0900

    feat(api): Swagger 통합 및 API 문서화
    - @nestjs/swagger 패키지 추가
    - API 엔드포인트에 Swagger 데코레이터 추가하여 문서화
    - EarnDto 및 UserDto에 ApiProperty 추가
    - PointHistoryEntity 및 UserEntity에 Swagger 관련 메타데이터 추가
    - main.ts에서 Swagger 설정 및 API 문서화 경로 추가

--- src/api/user/dto/user.dto.ts
+++ src/api/user/dto/user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({ description: '사용자 ID', example: 'testuser' })
   @IsString()
   id: string;
 
+  @ApiProperty({ description: '사용자 비밀번호', example: 'password123' })
   @IsString()
   password: string;
 }
